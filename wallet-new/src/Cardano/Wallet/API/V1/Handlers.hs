28a3a5583c89e1233d0d28a4e2479677dc02c03a
Alfredo Di Napoli <alfredo.dinapoli@iohk.io>
2018-07-19 17:16:09 +0200

    [CBR-342] Implements the 'createAddress' functionality
    This commit introduces the 'createAddress' functionality end to end, by
    providing a new Servant Handler, a new WalletLayer endpoint and the
    relevant Kernel function.
    
    No tests are provided yet.

--- wallet-new/src/Cardano/Wallet/API/V1/Handlers.hs
+++ wallet-new/src/Cardano/Wallet/API/V1/Handlers.hs
@@ -4,6 +4,7 @@ import           Servant
 import           Universum
 
 import qualified Cardano.Wallet.API.V1 as V1
+import qualified Cardano.Wallet.API.V1.Handlers.Accounts as Accounts
 import qualified Cardano.Wallet.API.V1.Handlers.Addresses as Addresses
 import qualified Cardano.Wallet.API.V1.Handlers.Transactions as Transactions
 import qualified Cardano.Wallet.API.V1.Handlers.Wallets as Wallets
@@ -16,14 +17,13 @@ import           Pos.Core.Mockable
 handlers :: ActiveWalletLayer Production -> Server V1.API
 handlers w =  Addresses.handlers passiveWallet
          :<|> Wallets.handlers   passiveWallet
-         :<|> accounts
+         :<|> Accounts.handlers passiveWallet
          :<|> Transactions.handlers w
          :<|> settings
          :<|> info
   where
     passiveWallet = walletPassiveLayer w
 
-    accounts = todo
     settings = todo
     info = todo
 
